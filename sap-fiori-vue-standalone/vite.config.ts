d405cc9a83ef23ba097e6d182b1347f6f6a06936
meta-d <meta.digital.cloud@gmail.com>
2023-10-26 10:06:57 +0800

    feat: config base url sap ui5_ui5

--- sap-fiori-vue-standalone/vite.config.ts
+++ sap-fiori-vue-standalone/vite.config.ts
@@ -5,7 +5,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   cacheDir: './node_modules/.vite/sap-fiori-vue-standalone',
-
+  base: '/sap/bc/ui5_ui5/sap/',
   server: {
     port: 4200,
     host: 'localhost',
