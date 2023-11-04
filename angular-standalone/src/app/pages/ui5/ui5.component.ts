34fb9827de56d7f9005c9c38336b9da98febc30a
meta-d <meta.digital.cloud@gmail.com>
2023-11-05 01:36:10 +0800

    feat: recreate iframe when url changed

--- angular-standalone/src/app/pages/ui5/ui5.component.ts
+++ angular-standalone/src/app/pages/ui5/ui5.component.ts
@@ -1,49 +1,64 @@
-import { SafePipe } from '@/app/core/pipes'
 import { FioriLaunchpadService, ToggleFullscreenDirective } from '@/app/core/'
+import { SafePipe } from '@/app/core/pipes'
 import { ZngAntdModule } from '@/app/core/shared.module'
 import { CommonModule } from '@angular/common'
-import { Component, computed, effect, inject } from '@angular/core'
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren, computed, effect, inject } from '@angular/core'
 import { toSignal } from '@angular/core/rxjs-interop'
 import { FormsModule } from '@angular/forms'
 import { ActivatedRoute, RouterModule } from '@angular/router'
-import { distinctUntilChanged, map, of, switchMap } from 'rxjs'
+import { TranslateModule } from '@ngx-translate/core'
 import { CookieService } from 'ngx-cookie-service'
 import { NGXLogger } from 'ngx-logger'
-import { TranslateModule } from '@ngx-translate/core'
-
+import { distinctUntilChanged, map, of, startWith, switchMap } from 'rxjs'
 
 @Component({
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule, TranslateModule, ZngAntdModule, SafePipe, ToggleFullscreenDirective],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    TranslateModule,
+    ZngAntdModule,
+    SafePipe,
+    ToggleFullscreenDirective
+  ],
   selector: 'zng-ui5',
   templateUrl: './ui5.component.html',
   styleUrls: ['./ui5.component.scss']
 })
-export class Ui5Component {
+export class Ui5Component implements AfterViewInit {
   private flpService = inject(FioriLaunchpadService)
   private route = inject(ActivatedRoute)
   private cookieService = inject(CookieService)
   private logger = inject(NGXLogger)
 
-  public semanticObject = toSignal(this.route.paramMap.pipe(
-    map((params) => params.get('id') as string),
-    distinctUntilChanged()
-  ))
+
+  @ViewChildren('appiframe') apps: QueryList<ElementRef> | undefined
+
+
+  public semanticObject = toSignal(
+    this.route.paramMap.pipe(
+      map((params) => params.get('id') as string),
+      distinctUntilChanged()
+    )
+  )
 
   public semanticTargetUrl = computed(() => {
     if (this.semanticObject()) {
       const fragment = this.route.snapshot.fragment
       const sapUserContext = this.cookieService.get('sap-usercontext')
-      return fragment ? `/sap/bc/ui2/flp${sapUserContext ? '?' + sapUserContext : ''}#${fragment}` : null
+      return fragment ? [`/sap/bc/ui2/flp${sapUserContext ? '?' + sapUserContext : ''}#${fragment}`] : null
     }
     return null
   })
 
-  readonly chips = toSignal(this.route.paramMap.pipe(
-    map((params) => params.get('group') as string),
-    distinctUntilChanged(),
-    switchMap((id) => id ? this.flpService.selectGroupChips(id) : of([])),
-  ))
+  readonly chips = toSignal(
+    this.route.paramMap.pipe(
+      map((params) => params.get('group') as string),
+      distinctUntilChanged(),
+      switchMap((id) => (id ? this.flpService.selectGroupChips(id) : of([])))
+    )
+  )
 
   constructor() {
     effect(() => {
@@ -52,5 +67,29 @@ export class Ui5Component {
       }
     })
   }
- 
+
+  ngAfterViewInit(): void {
+    if (this.apps) {
+      this.apps.changes.pipe(startWith(this.apps.toArray())).subscribe((apps: ElementRef[]) => {
+        apps.forEach((app) => this.styleFioriApp(app.nativeElement))
+      })
+    }
+  }
+
+  private styleFioriApp(iframe: HTMLIFrameElement) {
+    iframe.onload = () => {
+      // Hidden Fiori App shell header
+      const style = document.createElement('style')
+      style.setAttribute('type', 'text/css')
+      style.appendChild(
+        document.createTextNode(
+          `#shell-hdr.sapUshellShellHead { display: none !important; } #shell-cntnt.sapUshellShellCanvas {top: 10px;} .zng-layout__header {display: none;}`
+        )
+      )
+
+      if (iframe.contentDocument) {
+        iframe.contentDocument.head.appendChild(style)
+      }
+    }
+  }
 }
