9d8b946a61f9902be521014f07e9e422e92ceaeb
Tiven.Wang <i.tiven.wang@gmail.com>
2023-12-11 17:05:09 +0800

    feat: fixed sap webgui not destroy bug: open in new window

--- angular-standalone/src/app/components/UI5App/app.component.ts
+++ angular-standalone/src/app/components/UI5App/app.component.ts
@@ -7,6 +7,8 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
+  Input,
+  OnDestroy,
   QueryList,
   ViewChildren,
   computed,
@@ -19,7 +21,6 @@ import { FormsModule } from '@angular/forms'
 import { Title } from '@angular/platform-browser'
 import { ActivatedRoute, RouterModule } from '@angular/router'
 import { TranslateModule } from '@ngx-translate/core'
-import { CookieService } from 'ngx-cookie-service'
 import { NGXLogger } from 'ngx-logger'
 import { EMPTY, distinctUntilChanged, map, startWith } from 'rxjs'
 
@@ -41,14 +42,25 @@ import { EMPTY, distinctUntilChanged, map, startWith } from 'rxjs'
     fadeAnimation
   ]
 })
-export class Ui5AppComponent implements AfterViewInit {
+export class UI5AppComponent implements AfterViewInit, OnDestroy {
   private flpService = inject(FioriLaunchpadService)
   private route = inject(ActivatedRoute)
-  private cookieService = inject(CookieService)
   private title = inject(Title)
   private logger = inject(NGXLogger)
 
+  @Input('sap-ui-tech-hint') sapUiTechHint = null
+  
   @ViewChildren('appiframe') apps!: QueryList<ElementRef>
+
+  /**
+   * Get semantic object id from param ':id' or current path
+   */
+  public semanticObject = toSignal(
+    this.route.paramMap.pipe(
+      map((params) => params.get('id') || this.route.snapshot.url[0]?.path as string),
+      distinctUntilChanged(),
+    )
+  )
 
   public groupId = toSignal(
     this.route.parent?.paramMap.pipe(
@@ -56,18 +68,6 @@ export class Ui5AppComponent implements AfterViewInit {
       distinctUntilChanged()
     ) ?? EMPTY
   )
-
-  public semanticObject = toSignal(
-    this.route.paramMap.pipe(
-      map((params) => params.get('id') as string),
-      distinctUntilChanged()
-    )
-  )
-
-  readonly appGroup = computed(() => {
-    const groupId = this.groupId()
-    return groupId ? this.flpService.getGroup(groupId) : null
-  })
 
   readonly chip = computed(() => {
     const soUrl = this.semanticObject()
@@ -79,34 +79,45 @@ export class Ui5AppComponent implements AfterViewInit {
     return null
   })
 
-  public semanticTargetUrl = computed(() => {
+  readonly chipIsGui = computed(() => this.sapUiTechHint === 'GUI' ||
+    this.chip()?.navigationSemanticParameters?.['sap-ui-tech-hint'] === 'GUI'
+  )
+
+  readonly #semanticTargetUrl = computed(() => {
     if (this.semanticObject()) {
-      const fragment = this.route.snapshot.fragment || this.chip()?.navigationTargetUrl
+      const fragment = this.route.snapshot.fragment || this.chip()?.navigationTargetUrl || this.semanticObject()
       const sapUserContext = this.flpService.getUserContext()
       return fragment
-        ? [`/sap/bc/ui2/flp${sapUserContext ? '?' + sapUserContext : ''}#${fragment}`]
-        : []
+        ? `/sap/bc/ui2/flp${sapUserContext ? '?' + sapUserContext : ''}#${fragment}`
+        : null
     }
     return null
+  })
+
+  iframeUrls = computed(() => {
+    if (this.semanticObject()) {
+      if (!this.chipIsGui() && this.#semanticTargetUrl()) {
+        return [this.#semanticTargetUrl()]
+      } else {
+        return []
+      }
+    } else {
+      return null
+    }
   })
 
   readonly appFullscreen = signal(false)
 
   constructor() {
     effect(() => {
-      if (this.semanticObject()) {
-        this.logger.debug(`open ui5 app for id: ${this.semanticObject()}`)
-      }
-      if (this.chip()) {
-        this.logger.debug(`find chip for ui5 app:`, this.chip())
-      }
-      if (this.semanticTargetUrl()) {
-        this.logger.debug(`open ui5 app: ${this.semanticTargetUrl()}`)
+      const semanticTargetUrl = this.#semanticTargetUrl()
+      if (this.chipIsGui() && semanticTargetUrl) {
+        window.open(semanticTargetUrl, '_self')
       }
     })
 
     effect(() => {
-      this.title.setTitle(this.chip()?.title ?? this.appGroup()?.title ?? this.title.getTitle())
+      this.title.setTitle(this.chip()?.title ?? this.title.getTitle())
     })
   }
 
@@ -135,5 +146,9 @@ export class Ui5AppComponent implements AfterViewInit {
 
   toggleFullscreen() {
     this.appFullscreen.update((value) => !value)
+  }
+
+  ngOnDestroy() {
+    //
   }
 }
--- angular-standalone/src/app/core/types.ts
+++ angular-standalone/src/app/core/types.ts
@@ -24,5 +24,25 @@ export interface Chip {
     searchKeywords: string;
     navigationSemanticObject: string;
     navigationSemanticAction: string;
+    navigationSemanticParameters: Record<string, string> | null
     navigationTargetUrl: string;
+}
+
+
+/*
+ * 菜单
+ * */
+export interface Menu {
+  id: number | string
+  fatherId: number | string
+  path: string
+  menuName: string
+  menuType: 'C' | 'F' // c:菜单，f按钮
+  icon?: string // 如果showIcon为false，设置这个为搜索窗口时，最左侧的icon
+  alIcon?: string // 如果showIcon为false，设置这个为搜索窗口时，最左侧的icon
+  open?: boolean
+  selected?: boolean // 是否选中
+  children?: Menu[]
+  code?: string // 权限码
+  newLinkFlag?: 0 | 1 // 是否是新页
 }
